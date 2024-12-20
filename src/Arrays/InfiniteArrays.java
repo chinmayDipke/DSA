e44d1948499dcaf1acac90e5c30504eb7a6052c2
Chinmay <dipkechinmay@gmail.com>
2024-12-21 00:40:28 +0530

    commit

--- src/Arrays/InfiniteArrays.java
+++ src/Arrays/InfiniteArrays.java
@@ -2,25 +2,27 @@ package Arrays;
 
 public class InfiniteArrays {
     public static void main(String[] args) {
-        int[] arr = {3, 5, 7, 9, 10, 90,
-                100, 130, 140, 160, 170};
-        int target = 12;
-        System.out.println(ans(arr, target));
+        int[] arr= { 2,3,5,6,8,9,10,13 };
+        int target = 45;
+        System.out.println(areyfind(arr, target));
     }
-    static int ans(int[] arr, int target) {
+    static int areyfind(int[] arr, int target) {
         int start = 0;
         int end = 1;
 
-
         while (target > arr[end]) {
-            int temp = end + 1;
 
-            end = end + (end - start + 1) * 2;
-            start = temp;
+            int newstart = end + 1 ;
+
+            end = end + (end - start + 1) * 2 ;
+
+            start = newstart;
         }
         return binarySearch(arr, target, start, end);
 
-    }
+        }
+
+
     static int binarySearch(int[] arr, int target, int start, int end) {
         while(start <= end) {
              int mid = start + (end - start) / 2;
