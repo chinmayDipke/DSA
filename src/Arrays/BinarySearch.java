b263fd42ac23a73fdd389099e3f49ccd0c97b7c1
chinmayDipke <chinmay.dipke19@gmail.com>
2024-12-26 22:17:46 +0530

    Binary Search

--- src/Arrays/BinarySearch.java
+++ src/Arrays/BinarySearch.java
@@ -1,4 +1,34 @@
 package Arrays;
 
+import java.util.Arrays;
+
 public class BinarySearch {
+    public static void main(String[] args) {
+        int[] arr= { 2,3,5,6,8,9,10,13 };
+
+        System.out.println(binarysearch(arr,6));
+    }
+    static int binarysearch(int[] arr, int target){
+        int start = 0;
+        int end = arr.length - 1;
+
+
+        while(start <= end){
+
+            int mid = start + (end - start) / 2;
+            if(target < arr[mid]){
+                end = mid - 1;
+            }
+            else if (target > arr[mid]){
+                start = mid + 1;
+            }
+            else{
+                return mid;
+            }
+
+        }
+        return -1;
+    }
+
+
 }
