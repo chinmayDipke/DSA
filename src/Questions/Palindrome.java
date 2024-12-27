61229f1af5e94bf756d8e7be7215d8c9585c8e8f
Chinmay <dipkechinmay@gmail.com>
2024-12-28 00:29:27 +0530

    commit

--- src/Questions/Palindrome.java
+++ src/Questions/Palindrome.java
@@ -3,9 +3,12 @@ package Questions;
 public class Palindrome {
     public static void main(String[] args) {
         int x = 1221;
-        System.out.println(isPalindrome(x));
+        System.out.println(isPalindrome2(x));
     }
     static boolean isPalindrome(int x) {
+        if(x<0){
+            return false;
+        }
         String s = Integer.toString(x);
         int len = s.length();
         for(int i = 0;i < len/2; i++){
@@ -15,5 +18,23 @@ public class Palindrome {
             }
         }
         return true;
+    }
+    static boolean isPalindrome2(int x) {
+        if(x<0){
+            return false;
+        }
+        int val = 0, sum = 0, num = x;
+
+        while(x > 0){
+            val = num % 10;
+            sum = sum * 10 + val;
+            num /= 10;
+
+        }
+
+            return (sum == num);
+
+
+
     }
 }
