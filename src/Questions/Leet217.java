57d3d66bc83e0211d02f0ec1feca0d8f48e5c546
Chinmay <dipkechinmay@gmail.com>
2024-12-30 00:01:35 +0530

    commit

---
+++ src/Questions/Leet217.java
@@ -0,0 +1,22 @@
+package Questions;
+
+import java.util.Arrays;
+
+public class Leet217 {
+    public static void main(String[] args) {
+        int[] arr = { 5, 6, 1, 3, 0 };
+
+
+        System.out.println(containsDuplicate(arr));
+    }
+
+    static boolean containsDuplicate(int[] arr) {
+        Arrays.sort(arr);
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] == arr[i - 1]) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
