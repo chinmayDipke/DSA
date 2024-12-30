9457ccd42cff946d639ab28e73dcfd81a91dfea9
Chinmay <dipkechinmay@gmail.com>
2024-12-30 23:36:18 +0530

    commit

---
+++ src/Sorting/InsertionSort.java
@@ -0,0 +1,25 @@
+package Sorting;
+
+import java.util.Arrays;
+
+public class InsertionSort {
+    public static void main(String[] args) {
+        int[] arr = { 5,7,1,0,2,1 };
+
+
+        insertionSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+    static void insertionSort(int[] arr) {
+
+        for (int i = 0; i < arr.length - 2; i++) {
+            for (int j = i+1; j < arr.length - 1; j++) {
+                if(arr[i] > arr[j]){
+                    int temp = arr[i];
+                    arr[i]=arr[j];
+                    arr[j]=temp;
+                }
+            }
+        }
+    }
+}
