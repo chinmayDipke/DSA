0318a24cd26ad3cf8f7157df771dd6cc6d554c4e
Chinmay <dipkechinmay@gmail.com>
2024-12-30 23:37:53 +0530

    commit

--- src/Sorting/InsertionSort.java
+++ src/Sorting/InsertionSort.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class InsertionSort {
     public static void main(String[] args) {
-        int[] arr = { 5,7,1,0,2,1 };
+        int[] arr = { 5,7,1,0,2,1,-1 };
 
 
         insertionSort(arr);
@@ -13,7 +13,7 @@ public class InsertionSort {
     static void insertionSort(int[] arr) {
 
         for (int i = 0; i < arr.length - 2; i++) {
-            for (int j = i+1; j < arr.length - 1; j++) {
+            for (int j = i+1; j <= arr.length - 1; j++) {
                 if(arr[i] > arr[j]){
                     int temp = arr[i];
                     arr[i]=arr[j];
