22ccd6bf5c2c498043ee06d43094477480a6cebb
chinmayDipke <chinmay.dipke19@gmail.com>
2024-12-31 23:59:41 +0530

    commit

---
+++ src/Sorting/CyclicSort.java
@@ -0,0 +1,33 @@
+package Sorting;
+
+import java.util.Arrays;
+
+public class CyclicSort {
+    public static void main(String[] args) {
+        int[] arr = { 3,4,5,1,2 };
+
+
+        cyclicSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static void cyclicSort(int[] arr) {
+       int i = 0;
+        while(i < arr.length){
+            int index = arr[i] - 1;
+            if (arr[i] != arr[index]) {
+                swap(arr, i, index);
+            } else{
+                i++;
+            }
+        }
+
+
+    }
+
+    static void swap(int[] arr, int start, int end) {
+        int temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+    }
+}
