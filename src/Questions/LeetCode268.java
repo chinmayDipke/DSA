909eee6c2d6b2fc952f57a71c1e79a6f2da26793
chinmayDipke <chinmay.dipke19@gmail.com>
2025-01-02 00:13:13 +0530

    commit

--- src/Questions/LeetCode268.java
+++ src/Questions/LeetCode268.java
@@ -4,27 +4,20 @@ import java.util.Arrays;
 
 public class LeetCode268 {
     public static void main(String[] args) {
-        int[] arr = { 5, 6, 1, 3, 0 };
-
-
-        missingNumber(arr);
-        System.out.println(Arrays.toString(arr));
+        int[] arr = {3, 0, 1};
+        int missing = missingNumber(arr);
+        System.out.println("Missing number: " + missing);
     }
 
+    static int missingNumber(int[] arr) {
+        int n = arr.length;
+        int expectedSum = n * (n + 1) / 2;
+        int actualSum = 0;
 
-        static int missingNumber(int[] arr) {
-            Arrays.sort(arr);
-            int i = 0;
-
-            int index = arr[i] - 1;
-            for(i = 1; i<arr.length;i++){
-                if(arr[i] != arr[index]){
-                    return index;
-                }
-            }
-            return -1;
+        for (int num : arr) {
+            actualSum += num;
         }
 
-
-
+        return expectedSum - actualSum;
+    }
 }
