35d645710cf0e82604165d8c0ca5c3cf12b74a25
chinmayDipke <chinmay.dipke19@gmail.com>
2025-01-02 01:29:19 +0530

    commit

---
+++ src/Questions/LeetCode69.java
@@ -0,0 +1,24 @@
+package Questions;
+
+import java.sql.SQLOutput;
+
+//https://leetcode.com/problems/sqrtx/
+public class LeetCode69 {
+
+    public static void main(String[] args) {
+        int x= 9;
+        System.out.println(mySqrt(x));
+    }
+    static int mySqrt(int x) {
+        if(x==0||x==1){
+            return x;
+        }
+        for(int i=1;i<=x;i++){
+            if(i*i>x){
+                return i-1;
+            }
+
+        }
+        return -1;
+    }
+}
