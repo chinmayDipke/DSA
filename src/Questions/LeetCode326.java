0501701ba7ff11bdc3368246052133c7a814dca7
chinmayDipke <chinmay.dipke19@gmail.com>
2025-01-04 01:19:58 +0530

    commit

---
+++ src/Questions/LeetCode326.java
@@ -0,0 +1,22 @@
+package Questions;
+
+public class LeetCode326 {
+    public static void main(String[] args) {
+
+        int n = 3, x;
+
+        for(int i = 0; i<=n; i++){
+
+        }
+        if(n>1)
+            System.out.println(3^n);
+    }
+   /* public boolean isPowerOfThree(int n) {
+        if(n==0)
+            return false;
+        if(n==1)
+            return true;
+
+        return true;
+    }*/
+}
---
+++ src/Recusion/Fibonacci.java
@@ -0,0 +1,24 @@
+package Recusion;
+
+import java.util.Arrays;
+
+public class Fibonacci {
+    public static void main(String[] args) {
+        int x = 7;
+        int[] arr=null;
+        // int[] arr;
+       // System.out.println(Arrays.toString(fibo(arr,x)));
+       /* int n=0, n1 = 1;
+        int n2 ;
+        System.out.println(n);
+        System.out.println(n1);
+        for(int i =2; i<=x; i++) {
+            n2 = n + n1;
+            System.out.println(n2);
+            n = n1;
+            n1 = n2;*/
+        }
+
+    }
+
+
