fdaffc9b735b4c6bba9982ca84838cbd82c98505
Chinmay <dipkechinmay@gmail.com>
2025-01-16 01:19:31 +0530

    commit

--- src/Questions/LeetCode292.java
+++ src/Questions/LeetCode292.java
@@ -2,10 +2,14 @@ package Questions;
 
 public class LeetCode292 {
     public static void main(String[] args) {
-        System.out.println(canWinNim(4));
+        System.out.println(canWinNim1(4));
     }
     static boolean canWinNim(int n) {
         return (n % 4 != 0);
-
+    }
+    static boolean canWinNim1(int n) {
+        if(n % 4 == 0)
+            return false;
+        else return true;
     }
 }
