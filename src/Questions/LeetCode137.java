62e4cb5b72f2be3d2601379f5100bd5fc489b76d
Chinmay <dipkechinmay@gmail.com>
2025-01-18 00:14:14 +0530

    commit

---
+++ src/Questions/LeetCode137.java
@@ -0,0 +1,18 @@
+package Questions;
+
+public class LeetCode137 {
+    public static void main(String[] args) {
+        int[] arr = {2,2,3,2};
+        System.out.println(singleNumber(arr));
+    }
+    static int singleNumber(int[] nums) {
+
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length - 1; i += 3) {
+            if (nums[i] != nums[i + 1]) {
+                return nums[i];
+            }
+        }
+        return nums[nums.length - 1];
+    }
+}
