9882c8235235f8cdc0fc4ac809ad529ca626b269
chinmayDipke <chinmay.dipke19@gmail.com>
2025-01-23 23:55:36 +0530

    commit

--- src/Questions/LeetCode1.java
+++ src/Questions/LeetCode1.java
@@ -6,6 +6,7 @@ public class LeetCode1 {
     public static void main(String[] args) {
         int[] nums = {2,7,11,15};
         System.out.println(Arrays.toString(twoSum(nums, 9)));
+        System.out.println(Arrays.toString(twoSum1(nums, 9)));
     }
     static int[] twoSum(int[] nums, int target) {
         int n = nums.length;
@@ -20,4 +21,18 @@ public class LeetCode1 {
         }
         return temp;
     }
+
+        static int[] twoSum1(int[] nums, int target) {
+            int n = nums.length;
+
+            for (int i = 0; i < n - 1; i++) {
+                for (int j = i + 1; j < n; j++) {
+                    if (nums[i] + nums[j] == target) {
+                        return new int[]{i, j};
+                    }
+                }
+            }
+            return new int[]{};
+        }
+
 }
