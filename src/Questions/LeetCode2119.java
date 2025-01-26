f29b58350830eca5132c42e31604ec997a76a033
chinmayDipke <chinmay.dipke19@gmail.com>
2025-01-27 00:50:54 +0530

    commit

---
+++ src/Questions/LeetCode2119.java
@@ -0,0 +1,24 @@
+package Questions;
+
+public class LeetCode2119 {
+    public static void main(String[] args) {
+        System.out.println(isSameAfterReversals(526));
+    }
+
+    private static boolean isSameAfterReversals(int num) {
+        int x = num % 10;
+
+        if(num==0){
+            return true;
+        }
+
+        if(x==0){
+            return false;
+        }
+
+
+
+
+        return true;
+    }
+}
