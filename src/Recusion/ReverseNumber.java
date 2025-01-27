b88164f7bcfe32206ee9591cb4a4db91058bdbd8
Chinmay <dipkechinmay@gmail.com>
2025-01-27 23:58:11 +0530

    commit

--- src/Recusion/ReverseNumber.java
+++ src/Recusion/ReverseNumber.java
@@ -2,16 +2,13 @@ package Recusion;
 
 public class ReverseNumber {
     public static void main(String[] args) {
-        System.out.println(reverse(1234));
+        System.out.println(reverse(12));
     }
 
     static int reverse(int num) {
-
         if (num < 10) {
             return num;
         }
-        int temp = num % 10;
-        int sum = temp * 10;
-        return reverse(num / 10 + sum);
+        return (num % 10) * 10 + reverse(num / 10);
     }
 }
