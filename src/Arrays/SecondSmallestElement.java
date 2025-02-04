4c4aa866ca51f427333c2112debda4bc33be4b7a
Chinmay <dipkechinmay@gmail.com>
2025-02-05 01:46:20 +0530

    commit

--- src/Arrays/SecondSmallestElement.java
+++ src/Arrays/SecondSmallestElement.java
@@ -1,29 +1,11 @@
 package Arrays;
 
-public class SecondLargestElement {
+public class SecondSmallestElement {
     public static void main(String[] args) {
         int[] arr = {2,5,3,12,5,9};
 
-        System.out.println(secondLargestElement(arr));
         System.out.println(secondSmallestElement(arr));
     }
-    static int secondLargestElement(int[] nums){
-        int largestElement = -1;
-
-        for(int i = 0; i < nums.length; i++ ){
-            if(nums[i] > largestElement ){
-                largestElement = nums[i];
-            }
-        }
-        int secondlargest = -1;
-        for(int i = 0; i < nums.length; i++ ){
-            if(nums[i] > secondlargest && nums[i] != largestElement){
-                secondlargest = nums[i];
-            }
-        }
-        return secondlargest;
-    }
-
     static int secondSmallestElement(int[] nums){
         int smallestElement = 99;
 
