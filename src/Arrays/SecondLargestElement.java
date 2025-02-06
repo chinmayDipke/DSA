8d907e624b0616e6cf9ebde78584cb327e4981f7
chinmayDipke <chinmay.dipke19@gmail.com>
2025-02-07 01:17:24 +0530

    commit

--- src/Arrays/SecondLargestElement.java
+++ src/Arrays/SecondLargestElement.java
@@ -3,11 +3,9 @@ package Arrays;
 public class SecondLargestElement {
     public static void main(String[] args) {
         int[] arr = {2,5,3,12,5,9};
-
-        System.out.println(secondLargestElement(arr));
-
+        System.out.println(secondLargestElement1(arr));
     }
-    static int secondLargestElement(int[] nums){
+    static int secondLargestElement1(int[] nums){
         int largestElement = -1;
 
         for(int i = 0; i < nums.length; i++ ){
@@ -22,5 +20,20 @@ public class SecondLargestElement {
             }
         }
         return secondlargest;
+    }
+
+    static int secondLargestElement(int[] nums){
+        int largest = -1, secondLargest = Integer.MIN_VALUE;
+
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] > largest) {
+                secondLargest = largest;
+                largest = nums[i];
+            }
+            else if(nums[i] < largest && nums[i] > secondLargest){
+                secondLargest = nums[i];
+            }
+        }
+        return secondLargest;
     }
 }
