d9a407b1600e9a176a997227128627b215c7209f
Chinmay <dipkechinmay@gmail.com>
2025-02-09 22:55:44 +0530

    commit

--- src/Questions/LeetCode1752.java
+++ src/Questions/LeetCode1752.java
@@ -1,9 +1,12 @@
 package Questions;
 
+import java.util.Arrays;
+
 public class LeetCode1752 {
     public static void main(String[] args) {
         int[] arr= {3,4,5,1,2};
         System.out.println(check(arr));
+        System.out.println(check1(arr));
     }
     public static boolean check(int[] nums) {
         int count = 0;
@@ -17,5 +20,29 @@ public class LeetCode1752 {
                 return false;
         }
         return true;
+    }
+    public static boolean check1(int[] nums) {
+        int n = nums.length;
+
+        int[] cloned = nums.clone();
+
+        Arrays.sort(cloned);
+
+        int k = 0;
+
+        while (k < n) {
+            int[] rotatedArray = new int[n];
+
+            for (int i = 0; i < n; i++) {
+                rotatedArray[(i + k) % n] = nums[i];
+            }
+
+            if (Arrays.equals(rotatedArray, cloned)) {
+                return true;
+            }
+
+            k++;
+        }
+        return false;
     }
 }
