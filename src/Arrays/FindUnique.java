9944b9342e2cbec6f2ec181376d128e4d2d4d3a4
chinmayDipke <chinmay.dipke19@gmail.com>
2025-02-15 00:13:36 +0530

    commit

--- src/Arrays/FindUnique.java
+++ src/Arrays/FindUnique.java
@@ -1,7 +1,4 @@
 package Arrays;
-
-
-
 public class FindUnique {
     public static void main(String[] args) {
         int[] arr = {2,3,5,2,3,5,6};
@@ -9,12 +6,9 @@ public class FindUnique {
     }
     static int duplicate(int[] arr){
         int unique = 0;
-
         for(int i = 0;i < arr.length;i++){
             unique = unique ^ arr[i];
         }
-
-
         return unique;
     }
 }
