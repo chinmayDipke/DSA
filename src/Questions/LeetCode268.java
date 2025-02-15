7dd62957958c0f4eeecb498bbc16bb479e96d0d0
chinmayDipke <chinmay.dipke19@gmail.com>
2025-02-16 00:43:40 +0530

    commit

--- src/Questions/LeetCode268.java
+++ src/Questions/LeetCode268.java
@@ -1,23 +1,17 @@
 package Questions;
-
-import java.util.Arrays;
-
 public class LeetCode268 {
     public static void main(String[] args) {
         int[] arr = {3, 0, 1};
         int missing = missingNumber(arr);
         System.out.println("Missing number: " + missing);
     }
-
     static int missingNumber(int[] arr) {
         int n = arr.length;
         int expectedSum = n * (n + 1) / 2;
         int actualSum = 0;
-
         for (int num : arr) {
             actualSum += num;
         }
-
         return expectedSum - actualSum;
     }
 }
