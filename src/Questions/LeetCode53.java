445b8caf88fee468693b8b6693b6ea94708377c3
Chinmay <dipkechinmay@gmail.com>
2025-02-22 00:05:44 +0530

    commit

---
+++ src/Questions/LeetCode53.java
@@ -0,0 +1,21 @@
+package Questions;
+class LeetCode53 {
+    public static void main(String[] args) {
+        int[] arr = {-2,1,-3,4,-1,2,1,-5,4};
+        System.out.println(maxSubArray(arr));
+    }
+    public static int maxSubArray(int[] nums) {
+        int max = Integer.MIN_VALUE, sum = 0;
+
+        for(int i = 0; i < nums.length; i++){
+
+            sum += nums[i];
+            max = Math.max(max, sum);
+
+            if(sum < 0){
+                sum = 0;
+            }
+        }
+        return max;
+    }
+}
