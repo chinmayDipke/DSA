fe9e30ec7cbea334d4b1d0804ec4cbc1baf32213
Chinmay <dipkechinmay@gmail.com>
2025-02-24 23:07:31 +0530

    commit

--- src/Questions/LeetCode53.java
+++ src/Questions/LeetCode53.java
@@ -6,7 +6,6 @@ class LeetCode53 {
     }
     public static int maxSubArray(int[] nums) {
         int max = Integer.MIN_VALUE, sum = 0;
-
         for(int i = 0; i < nums.length; i++){
             sum += nums[i];
             max = Math.max(max, sum);
