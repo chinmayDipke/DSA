1d081c9eaa6c70e86dc2f5e768fc2a86c801648f
Chinmay <dipkechinmay@gmail.com>
2025-02-24 23:06:58 +0530

    commit

--- src/Questions/LeetCode53.java
+++ src/Questions/LeetCode53.java
@@ -8,10 +8,8 @@ class LeetCode53 {
         int max = Integer.MIN_VALUE, sum = 0;
 
         for(int i = 0; i < nums.length; i++){
-
             sum += nums[i];
             max = Math.max(max, sum);
-
             if(sum < 0){
                 sum = 0;
             }
