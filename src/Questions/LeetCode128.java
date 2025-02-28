610be9eaf76605f4f1e1f04c2ab2603a461408db
Chinmay <dipkechinmay@gmail.com>
2025-02-28 22:32:42 +0530

    commit

--- src/Questions/LeetCode128.java
+++ src/Questions/LeetCode128.java
@@ -7,7 +7,6 @@ public class LeetCode128 {
         int[] arr = {100, 4, 200, 1, 3, 2};
         System.out.println(longestConsecutive(arr));
     }
-
     public static int longestConsecutive(int[] nums) {
         if (nums.length == 0) return 0;
         Arrays.sort(nums);
