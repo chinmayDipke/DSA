4ae3cc88ffdd07c63743ac70084231e5257893b1
Chinmay <chinmay.dipke19@gmail.com>
2025-03-02 00:26:24 +0530

    commit

--- src/Questions/LeetCode1.java
+++ src/Questions/LeetCode1.java
@@ -21,10 +21,8 @@ public class LeetCode1 {
         }
         return temp;
     }
-
         static int[] twoSum1(int[] nums, int target) {
             int n = nums.length;
-
             for (int i = 0; i < n - 1; i++) {
                 for (int j = i + 1; j < n; j++) {
                     if (nums[i] + nums[j] == target) {
