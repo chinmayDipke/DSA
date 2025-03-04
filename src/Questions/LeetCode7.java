ebc2f9e2e9f40f8c3bd22f082bdf3c72feba746d
Chinmay <dipkechinmay@gmail.com>
2025-03-04 23:05:33 +0530

    commit

--- src/Questions/LeetCode7.java
+++ src/Questions/LeetCode7.java
@@ -3,7 +3,9 @@ package Questions;
 public class LeetCode7 {
     public static void main(String[] args) {
         System.out.println(reverse(120));
+        System.out.println(reverseInt(120));
     }
+    static int res = 0;
     static int reverse(int x) {
         double result = 0;
         while (x != 0)
@@ -16,5 +18,12 @@ public class LeetCode7 {
             x = x / 10;
         }
         return (int)result;
+    }
+    public static int reverseInt(int num) {
+        if (num < 10) {
+            return res * 10 + num;
+        }
+        res = res * 10 + num % 10;
+        return reverse(num / 10);
     }
 }
