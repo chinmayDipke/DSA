d307939c40f5df0b874add264085000886a9d849
Chinmay <chinmay.dipke19@gmail.com>
2025-03-06 00:15:43 +0530

    commit

--- src/Questions/LeetCode73.java
+++ src/Questions/LeetCode73.java
@@ -1,35 +1,44 @@
 package Questions;
 
+import java.util.Arrays;
+
 public class LeetCode73 {
     public static void main(String[] args) {
-        int[][] mat = {[0,1,2,0],[3,4,5,2],[1,3,1,5]};
-        System.out.println(setZeroes(mat));
+        int[][] mat = {{0,1,2,0},{3,4,5,2},{1,3,1,5}};
+
+        // Call the method
+        setZeroes(mat);
+
+        // Print the matrix correctly
+        for (int[] row : mat) {
+            System.out.println(Arrays.toString(row));
+        }
     }
 
-        public void setZeroes(int[][] matrix) {
-            int rows = matrix.length;
-            int cols = matrix[0].length;
+    public static void setZeroes(int[][] matrix) {
+        int rows = matrix.length;
+        int cols = matrix[0].length;
 
-            int[] row = new int[rows];
-            int[] col = new int[cols];
+        int[] row = new int[rows];
+        int[] col = new int[cols];
 
-
-            for(int i = 0; i < rows; i++) {
-                for(int j = 0; j < cols; j++) {
-                    if(matrix[i][j] == 0) {
-                        row[i] = 1;
-                        col[j] = 1;
-                    }
-                }
-            }
-
-
-            for(int i = 0; i < rows; i++) {
-                for(int j = 0; j < cols; j++) {
-                    if(row[i] == 1 || col[j] == 1) {
-                        matrix[i][j] = 0;
-                    }
+        // Mark the rows and columns that need to be zeroed
+        for(int i = 0; i < rows; i++) {
+            for(int j = 0; j < cols; j++) {
+                if(matrix[i][j] == 0) {
+                    row[i] = 1;
+                    col[j] = 1;
                 }
             }
         }
+
+        // Set the elements to zero based on the markers
+        for(int i = 0; i < rows; i++) {
+            for(int j = 0; j < cols; j++) {
+                if(row[i] == 1 || col[j] == 1) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
 }
