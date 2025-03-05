3b1facf36d5e63f47599a0cc5143f60c14dda925
Chinmay <dipkechinmay@gmail.com>
2025-03-06 00:15:28 +0530

    commit

---
+++ src/Questions/LeetCode54.java
@@ -0,0 +1,55 @@
+package Questions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LeetCode54 {
+    public static void main(String[] args) {
+        int[][] matrix = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}};
+        System.out.println(spiralOrder(matrix));
+    }
+
+    public static List<Integer> spiralOrder(int[][] matrix) {
+        List<Integer> result = new ArrayList<>();
+
+        // Edge case: Empty matrix
+        if (matrix == null || matrix.length == 0) {
+            return result;
+        }
+
+        int rows = matrix.length, cols = matrix[0].length;
+        int top = 0, bottom = rows - 1, left = 0, right = cols - 1;
+
+        while (top <= bottom && left <= right) {
+            // Traverse from left to right
+            for (int i = left; i <= right; i++) {
+                result.add(matrix[top][i]);
+            }
+            top++;
+
+            // Traverse from top to bottom
+            for (int i = top; i <= bottom; i++) {
+                result.add(matrix[i][right]);
+            }
+            right--;
+
+            // Traverse from right to left (only if there are remaining rows)
+            if (top <= bottom) {
+                for (int i = right; i >= left; i--) {
+                    result.add(matrix[bottom][i]);
+                }
+                bottom--;
+            }
+
+            // Traverse from bottom to top (only if there are remaining columns)
+            if (left <= right) {
+                for (int i = bottom; i >= top; i--) {
+                    result.add(matrix[i][left]);
+                }
+                left++;
+            }
+        }
+
+        return result;
+    }
+}
