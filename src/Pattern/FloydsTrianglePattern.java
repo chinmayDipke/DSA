a3704cb8ee70502025fa092db282279c225d8105
Chinmay <dipkechinmay@gmail.com>
2026-01-21 14:30:20 +0530

    Add FloydsTrianglePattern pattern implementation

---
+++ src/Pattern/FloydsTrianglePattern.java
@@ -0,0 +1,23 @@
+package Pattern;
+
+/*
+Pattern Printed:
+1
+2 3
+4 5 6
+7 8 9 10
+*/
+
+public class FloydsTrianglePattern {
+    public static void main(String[] args) {
+        int row = 5;
+        int num = 1;
+
+        for (int i = 1; i <= row; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(num++ + " ");
+            }
+            System.out.println();
+        }
+    }
+}
